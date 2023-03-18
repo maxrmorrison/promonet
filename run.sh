39ddc2e83fd2cdc6ac4d7a0c206ca310d62394f8
Max Morrison <max@descript.com>
2023-03-18 19:02:45 +0000

    Runfile updates

--- run.sh
+++ run.sh
@@ -12,24 +12,20 @@ python -m promonet.data.augment --gpu $1
 python -m promonet.data.preprocess --features spectrogram ppg prosody --gpu $1
 python -m promonet.partition
 
-# First pass experiment trainings
+# First pass experiments trainings and evaluations
 # python -m promonet.train --config config/base-small.py --gpus $1
 # python -m promonet.train --config config/conddisc-small.py --gpus $1
 # python -m promonet.train --config config/conddisc-condgen-small.py --gpus $1
 # python -m promonet.train --config config/conddisc-condgen-augment-small.py --gpus $1
 python -m promonet.train --config config/conddisc-condgen-augment-snake-small.py --gpus $1
 
-# Baseline trainings
+# Baseline trainings and evaluations
 # python -m promonet.train --config config/promovoco-small.py --gpus $1
 # python -m promonet.train --config config/spectral-small.py --gpus $1
 # python -m promonet.train --config config/spectral-voco-small.py --gpus $1
 # python -m promonet.train --config config/two-stage-small.py --gpus $1
 # python -m promonet.train --config config/vits.py --gpus $1
 
-# Evaluate
+# DSP-based baseline evaluations
 python -m promonet.evaluate --config config/psola.py
 python -m promonet.evaluate --config config/world.py
-python -m promonet.evaluate \
-    --config config/test-ablate-mrd-small.py \
-    --checkpoint runs/test-ablate-mrd-small/00250000.pt \
-    --gpu $1
