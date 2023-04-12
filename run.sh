15e52275edd86abb05a900bf2ed76b46a479caf5
Max Morrison <maxrmorrison@gmail.com>
2023-04-12 23:24:45 +0000

    Suppress matplotlib warning

--- run.sh
+++ run.sh
@@ -5,19 +5,19 @@
 # $1 - index of GPU to use
 
 # Download datasets
-python -m promonet.data.download
+# python -m promonet.data.download
 
 # Setup experiments
-python -m promonet.data.augment
-python -m promonet.data.preprocess --features spectrogram ppg prosody --gpu $1
-python -m promonet.partition
+# python -m promonet.data.augment
+# python -m promonet.data.preprocess --features spectrogram ppg prosody --gpu $1
+# python -m promonet.partition
 
 # First pass experiments trainings and evaluations
 python -m promonet.train --config config/base.py --gpus $1
 python -m promonet.train --config config/conddisc.py --gpus $1
 python -m promonet.train --config config/conddisc-condgen.py --gpus $1
 python -m promonet.train --config config/conddisc-condgen-augment.py --gpus $1
-python -m promonet.train --config config/conddisc-condgen-augment-snake.py --gpus $1
+# python -m promonet.train --config config/conddisc-condgen-augment-snake.py --gpus $1
 
 # Baseline trainings and evaluations
 # python -m promonet.train --config config/promovoco.py --gpus $1
