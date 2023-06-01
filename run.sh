4e2041d1d5cc22ea062da35e2931edc45a781acc
Max Morrison <maxrmorrison@gmail.com>
2023-06-01 10:33:32 +0000

    Vocoder fixes

--- run.sh
+++ run.sh
@@ -13,10 +13,10 @@
 # python -m promonet.partition
 
 # First pass experiments trainings and evaluations
-python -m promonet.train --config config/base.py --gpus $1
+# python -m promonet.train --config config/base.py --gpus $1
 # python -m promonet.train --config config/conddisc.py --gpus $1
 # python -m promonet.train --config config/conddisc-condgen.py --gpus $1
-python -m promonet.train --config config/conddisc-condgen-augment.py --gpus $1
+# python -m promonet.train --config config/conddisc-condgen-augment.py --gpus $1
 # python -m promonet.train --config config/conddisc-condgen-augment-snake.py --gpus $1
 
 # Baseline trainings and evaluations
@@ -26,5 +26,5 @@ python -m promonet.train --config config/two-stage.py --gpus $1
 python -m promonet.train --config config/vits.py --gpus $1
 
 # DSP-based baseline evaluations
-python -m promonet.evaluate --config config/psola.py --gpus $1
-python -m promonet.evaluate --config config/world.py --gpus $1
+# python -m promonet.evaluate --config config/psola.py --gpus $1
+# python -m promonet.evaluate --config config/world.py --gpus $1
