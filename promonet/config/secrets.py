37a171e81af7865606ef33286392655ef3e36e2b
Cameron <cameronchurchwell2024@u.northwestern.edu>
2023-08-10 14:19:19 -0500

    removed extraneous file

--- promonet/config/secrets.py
+++
@@ -1,7 +0,0 @@
-###############################################################################
-# Notification settings (apprise)
-###############################################################################
-NOTIFICATION_SERVICES = [
-    # 'https://discord.com/api/webhooks/1103526641466085547/GZ9Vlui-144AK5cDQNErhYpbsAo733CsaBWiihPCxIh0x2mf7wjRnmk928MgSTOUk5W7',
-    'https://hooks.slack.com/services/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'
-]
