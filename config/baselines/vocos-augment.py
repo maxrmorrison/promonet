2681b1e419b538d48ce432f4a666c2514b722354
Max <maxrmorrison@gmail.com>
2024-01-16 10:25:56 -0600

    Separate pitch distributions; hinge loss; evals

---
+++ config/baselines/vocos-augment.py
@@ -0,0 +1,42 @@
+import functools
+
+import torch
+
+MODULE = 'promonet'
+
+# Configuration name
+CONFIG = 'vocos-augment'
+
+# Whether to use hinge loss instead of L2
+ADVERSARIAL_HINGE_LOSS = True
+
+# Whether to use pitch augmentation
+AUGMENT_PITCH = True
+
+# Batch size
+BATCH_SIZE = 16
+
+# Input features
+INPUT_FEATURES = ['spectrogram']
+
+# Whether to use the multi-resolution spectrogram discriminator from UnivNet
+MULTI_RESOLUTION_DISCRIMINATOR = True
+
+# Whether to use the multi-scale waveform discriminator from MelGAN
+MULTI_SCALE_DISCRIMINATOR = False
+
+# Training optimizer
+OPTIMIZER = functools.partial(
+    torch.optim.AdamW,
+    lr=2e-4,
+    betas=(.9, .999),
+    eps=1e-9)
+
+# Only use spectral features
+SPECTROGRAM_ONLY = True
+
+# Number of training steps
+STEPS = 1000000
+
+# Number of neural network layers in Vocos
+VOCOS_LAYERS = 8
---
+++ config/baselines/vocos-batch64.py
@@ -0,0 +1,39 @@
+import functools
+
+import torch
+
+MODULE = 'promonet'
+
+# Configuration name
+CONFIG = 'vocos-batch64'
+
+# Whether to use hinge loss instead of L2
+ADVERSARIAL_HINGE_LOSS = True
+
+# Batch size
+BATCH_SIZE = 64
+
+# Input features
+INPUT_FEATURES = ['spectrogram']
+
+# Whether to use the multi-resolution spectrogram discriminator from UnivNet
+MULTI_RESOLUTION_DISCRIMINATOR = True
+
+# Whether to use the multi-scale waveform discriminator from MelGAN
+MULTI_SCALE_DISCRIMINATOR = False
+
+# Training optimizer
+OPTIMIZER = functools.partial(
+    torch.optim.AdamW,
+    lr=2e-4,
+    betas=(.9, .999),
+    eps=1e-9)
+
+# Only use spectral features
+SPECTROGRAM_ONLY = True
+
+# Number of training steps
+STEPS = 400000
+
+# Number of neural network layers in Vocos
+VOCOS_LAYERS = 8
